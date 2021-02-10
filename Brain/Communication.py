5e019bf556867beb1c1533db583996162a3595d7
Song <217.Songs.SH@gmail.com>
2021-02-10 21:50:24 +0900

    send state and    receive data convert float

--- Brain/Communication.py
+++ Brain/Communication.py
@@ -1,9 +1,11 @@
 from socket import *
 import threading
 import time
+import struct
 
 STOP_FLAG = False
 CNT = 0
+
 
 def send(sock):
     global STOP_FLAG
@@ -17,7 +19,6 @@ def send(sock):
         time.sleep(0.5)
 
 
-
 def receive(sock):
     global STOP_FLAG
     global CNT
@@ -26,12 +27,14 @@ def receive(sock):
 
         if recvData:
             CNT += 1
-            print('상대방 :', recvData.decode('utf-8'))
+
+            info = [recvData[i:i+4] for i in range(0, len(recvData), 4)]
+            info = [int(struct.unpack('<f', data)[0]) for data in info]
+            print(info)
         else:
             print('disconeced')
             STOP_FLAG = True
             break
-        time.sleep(0.5)
 
 
 port = 9999
@@ -40,7 +43,7 @@ serverSock = socket(AF_INET, SOCK_STREAM)
 serverSock.bind(('', port))
 serverSock.listen(1)
 
-print('%d번 포트로 접속 대기중...'%port)
+print('%d번 포트로 접속 대기중...' % port)
 
 connectionSock, addr = serverSock.accept()
 
@@ -56,5 +59,4 @@ while True:
     time.sleep(0.5)
     print(CNT)
     if STOP_FLAG:
-
         break
