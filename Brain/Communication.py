3eeef6731329ba57edac7d09006f582118cb9767
Song <217.Songs.SH@gmail.com>
2021-02-11 15:11:40 +0900

    send, receive data buffer

--- Brain/Communication.py
+++ Brain/Communication.py
@@ -5,32 +5,32 @@ import struct
 
 STOP_FLAG = False
 CNT = 0
+Receive_Buffer = []
+Send_Buffer = []
 
 
 def send(sock):
     global STOP_FLAG
-
+    global Send_Buffer
     while True:
-        sendData = '1'
-        sock.send(sendData.encode('utf-8'))
+        if len(Send_Buffer) > 0:
+            sock.send(Send_Buffer[0])
+            Send_Buffer.pop(0)
         if STOP_FLAG:
             break
-
-        time.sleep(0.5)
 
 
 def receive(sock):
     global STOP_FLAG
     global CNT
+    global Receive_Buffer
+
     while True:
         recvData = sock.recv(1024)
 
         if recvData:
             CNT += 1
-
-            info = [recvData[i:i+4] for i in range(0, len(recvData), 4)]
-            info = [int(struct.unpack('<f', data)[0]) for data in info]
-            print(info)
+            Receive_Buffer.append(recvData)
         else:
             print('disconeced')
             STOP_FLAG = True
@@ -55,8 +55,21 @@ receiver = threading.Thread(target=receive, args=(connectionSock,))
 sender.start()
 receiver.start()
 
+
+def BytesToState(data):
+    info = [data[i:i + 4] for i in range(0, len(data), 4)]
+    info = [int(struct.unpack('<f', data)[0]) for data in info]
+    print(info)
+
+
 while True:
     time.sleep(0.5)
     print(CNT)
+
+    Send_Buffer.append('1'.encode('utf-8'))
+
+    if len(Receive_Buffer) > 0:
+        BytesToState(Receive_Buffer[0])
+        Receive_Buffer.pop(0)
     if STOP_FLAG:
         break
