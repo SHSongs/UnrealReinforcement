f55c82a0d8864209e98adbe9d91d2774154b7e99
Song <217.Songs.SH@gmail.com>
2021-02-21 17:37:46 +0900

    import c++ vehicle template

--- URGame/Source/URGame/URGame.Build.cs
+++ URGame/Source/URGame/URGame.Build.cs
@@ -8,7 +8,7 @@ public class URGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PhysXVehicles"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
